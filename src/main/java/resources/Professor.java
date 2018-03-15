8ef56da27efc95f6ac3e60f89a666c8b31eaa3af
Marie-Lynne Block <marielynne.block@gmail.com>
2018-03-15 13:55:53 +0100

    Setup structure

---
+++ src/main/java/resources/Professor.java
@@ -0,0 +1,14 @@
+package resources;
+
+public class Professor {
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    public Professor(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+}
