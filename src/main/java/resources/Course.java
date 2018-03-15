d788358afd57934692cccc3a65d22e1e0202f0c9
Marie-Lynne Block <marielynne.block@gmail.com>
2018-03-15 13:58:52 +0100

    Create resources

--- src/main/java/resources/Course.java
+++ src/main/java/resources/Course.java
@@ -1,4 +1,48 @@
 package resources;
 
 public class Course {
+    private int id;
+    private int studyPoints;
+    private String Category;
+    private Professor professor;
+
+    public Course(int id, int studyPoints, String category, Professor professor) {
+        this.id = id;
+        this.studyPoints = studyPoints;
+        Category = category;
+        this.professor = professor;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getStudyPoints() {
+        return studyPoints;
+    }
+
+    public void setStudyPoints(int studyPoints) {
+        this.studyPoints = studyPoints;
+    }
+
+    public String getCategory() {
+        return Category;
+    }
+
+    public void setCategory(String category) {
+        Category = category;
+    }
+
+    public Professor getProfessor() {
+        return professor;
+    }
+
+    public void setProfessor(Professor professor) {
+        this.professor = professor;
+    }
+
 }
